83e478421b9db00ebd8f9940f037ae264ab79346
oshiyz <oshadhadahanayaka0504@gmail.com>
2025-03-23 09:42:29 +0530

    done

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AdminRoutingModule } from './admin-routing.module';
-import { TourPackageFormComponent } from './tour-package-form/tour-package-form.component';
 
 @NgModule({
   declarations: [
 
   ],
   imports: [
-    TourPackageFormComponent,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
